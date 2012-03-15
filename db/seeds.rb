98b587dd3feda58f59d2e4669a252f421e03c775
marcoo <marco.kolloeffel@googlemail.com>
2012-03-15 21:14:15 +0100

    added 2 page seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Page.create(:id => 1, :title => "welcome site", :content => "blubb")
+Page.create(:id => 2, :title => "imprint", :content => "bliblablub")
